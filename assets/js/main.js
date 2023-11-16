a57dfe9a019bc50ad6789778204b445038001740
ArunKumar000777 <arunashok1020@gmail.com>
2023-11-16 11:34:12 +0530

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,64 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+    navToggle = document.getElementById("nav-toggle"),
+    navClose = document.getElementById("nav-close");
 
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add("show-menu");
+    });
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose) {
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove("show-menu");
+    });
+}
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+    const navMenu = document.getElementById("nav-menu");
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
-
 /*=============== SWIPER SHOE ===============*/
-
+let swiperShoes = new Swiper(".home__swiper", {
+    loop: true,
+    spaceBetween: 32,
+    grabCursor: true,
+    effect: "creative",
+    creativeEffect: {
+        prev: {
+            translate: [-100, 0, -500],
+            opacity: 0,
+        },
+        next: {
+            translate: [100, 0, -500],
+            opacity: 0,
+        },
+    },
+    // If we need pagination
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
 
 /*=============== SHADOW HEADER ===============*/
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const shadowHeader = () => {
+    const header = document.getElementById("header");
+    // When the scroll is greater than 50 viewport height, add the shadow-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add("shadow-header") : header.classList.remove("shadow-header");
+};
+window.addEventListener("scroll", shadowHeader);
